4d96caf45633f27996265244fce7d54b51e3c421
mathiasdag <mathias@asso.gd>
2022-11-23 10:28:46 +0100

    ...

--- components/ListOfProducers/index.js
+++ components/ListOfProducers/index.js
@@ -7,7 +7,7 @@ export default function ListOfProducers({ list }) {
     .filter((a) => a.producers.length > 0)
     .map((item) => (
       <section key={item.slug} className="region h-[200em] w-full">
-        <Header className="sticky top-14 right-0 w-full px-4 text-center sm:top-6 sm:text-right">
+        <Header className="sticky top-12 right-0 w-full px-4 text-center sm:top-6 sm:text-right">
           <Heading as="h2" size="xl">
             {item.origin.region}, {item.origin.country}
           </Heading>
@@ -15,7 +15,7 @@ export default function ListOfProducers({ list }) {
         {item.producers.map((producer) => (
           <section key={producer.id} className="producer h-[200em]">
             <Header
-              className="sticky inset-x-0 top-[3.75em] p-8 text-center sm:top-8"
+              className="sticky inset-x-0 top-[3.25em] p-8 text-center sm:top-8"
               secondLevel={true}
             >
               <Heading as="h2" size="xl">

2f93c5e16d97699c752b652baee27e25f2858145
mathiasdag <mathias@asso.gd>
2023-01-31 16:18:10 +0100

    ...

--- slices/Image/index.js
+++ slices/Image/index.js
@@ -20,44 +20,42 @@ const Image = ({ slice, index }) => {
   const isHome = asPath === "/" || asPath === "/hem";
 
   return (
-    <AnimateInView>
-      <figure
-        className={clsx(
-          "relative",
-          isLandscape && "col-span-full mx-auto max-w-4xl",
-          query.aid && "mx-4",
-          isLandscape &&
-            query.aid &&
-            "sm:mx-[-4em] sm:rounded-md sm:bg-white sm:p-4",
-          !isLandscape && query.aid && "mx-12 sm:mx-36",
-          isHome && "mx-2 md:mx-0"
+    <figure
+      className={clsx(
+        "relative",
+        isLandscape && "col-span-full mx-auto max-w-4xl",
+        query.aid && "mx-4",
+        isLandscape &&
+          query.aid &&
+          "sm:mx-[-4em] sm:rounded-md sm:bg-white sm:p-4",
+        !isLandscape && query.aid && "mx-12 sm:mx-36",
+        isHome && "mx-2 md:mx-0"
+      )}
+    >
+      <LoadingAssetAnimation loaded={loaded}>
+        <Img
+          src={image.url}
+          width={image.dimensions.width}
+          height={image.dimensions.height}
+          alt={image.alt ?? "Ingen beskrivning tillgänglig"}
+          className={clsx(!query.aid && "h-full rounded-sm object-cover")}
+          onLoadingComplete={(e) => setLoaded(true)}
+        />
+        {caption && (
+          <figcaption
+            className={clsx(
+              query.aid || query.uid === "om-oss"
+                ? "mx-1 mt-1 text-center text-sm"
+                : "absolute inset-x-0 bottom-0 rounded-md bg-gradient-to-t from-black/80 p-6 pt-12 text-white"
+            )}
+          >
+            <div className="mx-auto">
+              <PrismicRichText field={caption} />
+            </div>
+          </figcaption>
         )}
-      >
-        <LoadingAssetAnimation loaded={loaded}>
-          <Img
-            src={image.url}
-            width={image.dimensions.width}
-            height={image.dimensions.height}
-            alt={image.alt ?? "Ingen beskrivning tillgänglig"}
-            className={clsx(!query.aid && "h-full rounded-sm object-cover")}
-            onLoadingComplete={(e) => setLoaded(true)}
-          />
-          {caption && (
-            <figcaption
-              className={clsx(
-                query.aid || query.uid === "om-oss"
-                  ? "mx-1 mt-1 text-center text-sm"
-                  : "absolute inset-x-0 bottom-0 rounded-md bg-gradient-to-t from-black/80 p-6 pt-12 text-white"
-              )}
-            >
-              <div className="mx-auto">
-                <PrismicRichText field={caption} />
-              </div>
-            </figcaption>
-          )}
-        </LoadingAssetAnimation>
-      </figure>
-    </AnimateInView>
+      </LoadingAssetAnimation>
+    </figure>
   );
 };
 

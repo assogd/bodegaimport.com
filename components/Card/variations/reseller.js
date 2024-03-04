50d9fb9077e2cba61a86e8fea20ba987f2a1d54f
mathiasdag <mathias@asso.gd>
2024-03-04 16:34:05 +0100

    remove old reseller code

--- components/Card/variations/reseller.js
+++ components/Card/variations/reseller.js
@@ -21,16 +21,6 @@ export default function Reseller({ item, size }) {
   const isSm = size === "sm";
   const { reseller, art_no, link, quantity, price, volume } = item;
 
-  const onlyPrice = price && !reseller && !art_no && !volume;
-
-  const priceAndQuantity = price && !reseller && !art_no && volume;
-  const priceAndReseller = price && reseller && !art_no && !volume;
-
-  const resellerAndQuantityAndPrice = price && reseller && !art_no && volume;
-  const artNoAndQuantityAndPrice = price && !reseller && art_no && volume;
-
-  const complete = price && reseller && art_no && volume;
-
   const params = [
     quantity,
     volume && `${volume} ml`,
@@ -68,87 +58,4 @@ export default function Reseller({ item, size }) {
       </a>
     )
   );
-
-  if (onlyPrice)
-    return (
-      <Container href={link.url}>
-        <div className="flex justify-between">
-          <div>(Styckpris)</div>
-          <div>{price} SEK</div>
-        </div>
-      </Container>
-    );
-
-  if (priceAndQuantity)
-    return (
-      <Container href={link.url}>
-        <div className="flex justify-between">
-          <div>
-            {quantity && `${quantity} x `}
-            {volume && `${volume} ml`}
-          </div>
-          <div>{price} SEK</div>
-        </div>
-      </Container>
-    );
-
-  if (priceAndReseller)
-    return (
-      <Container className="flex justify-between" href={link.url}>
-        <div className="flex justify-between">
-          <div>{reseller}</div>
-          <div>{price} SEK</div>
-        </div>
-      </Container>
-    );
-
-  if (resellerAndQuantityAndPrice)
-    return (
-      <Container href={link.url}>
-        <div>{reseller}</div>
-        <div className="flex justify-between">
-          <div>
-            {quantity && `${quantity} x `}
-            {volume && `${volume} ml`}
-          </div>
-          <div>{price} SEK</div>
-        </div>
-      </Container>
-    );
-
-  if (artNoAndQuantityAndPrice)
-    return (
-      <Container href={link.url}>
-        <div>Artikelnr {art_no}</div>
-        <div className="flex justify-between">
-          <div>
-            {quantity && `${quantity} x `}
-            {volume && `${volume} ml`}
-          </div>
-          <div>{price} SEK</div>
-        </div>
-      </Container>
-    );
-
-  if (complete)
-    return (
-      <Container href={link.url}>
-        <div className="flex justify-between">
-          <div>{reseller}</div>
-          <div>
-            {quantity && `${quantity} x `}
-            {volume && `${volume} ml`}
-          </div>
-        </div>
-        <div className="flex justify-between">
-          <div>
-            <div>Artikelnr {art_no}</div>
-          </div>
-          <div>{price} SEK</div>
-        </div>
-      </Container>
-    );
-
-  console.log(item);
-  return null;
 }

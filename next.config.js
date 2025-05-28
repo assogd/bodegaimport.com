80beb09308199afb9967a3eb15dda6f5b61654d7
mathiasdag <mathias@asso.gd>
2025-05-28 12:09:13 +0200

    feat: add wildcard for instagram cdn domain in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      'eu-west-2.graphassets.com',
-      'cdninstagram.com',
-      'scontent.cdninstagram.com',
-      'scontent-cph2-1.cdninstagram.com',
-    ],
+    domains: ['eu-west-2.graphassets.com', '*.cdninstagram.com'],
   },
 };
 
